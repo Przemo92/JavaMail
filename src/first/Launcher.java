f7bf0808b9e8b710cf507dd867736ec1fe64ef2e
Przemek Kapela <przmek.kapela92@gmail.com>
2020-09-02 16:57:53 +0200

    First commit

---
+++ src/first/Launcher.java
@@ -0,0 +1,24 @@
+package first;
+
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+Parent parent = FXMLLeader
+
+        Scene scene = Scene(stackPane, v: 300, v1: 250);
+        stage.setScene(scene);
+
+        stage.show();
+
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module java.mail {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.web;
+
+    opens first;
+}
