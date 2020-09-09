ccd2168f471bae619233033881de58deceffdc52
Przemek Kapela <przmek.kapela92@gmail.com>
2020-09-09 21:44:11 +0200

    commit again

--- src/barosanu/controller/EmailDetailsController.java
+++ src/barosanu/controller/EmailDetailsController.java
@@ -1,2 +1,50 @@
-package barosanu.controller;public class EmailDetailsController {
+package barosanu.controller;
+
+import barosanu.EmailMenager;
+import barosanu.controller.services.MessageRendererService;
+import barosanu.model.EmailMessage;
+import barosanu.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.web.WebView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class EmailDetailsController extends BaseController implements Initializable
+{
+
+    @FXML
+    private WebView webView;
+
+    @FXML
+    private Label attachmentLabel;
+
+    @FXML
+    private Label subjectLabel;
+
+    @FXML
+    private Label senderLabel;
+
+    @FXML
+    private HBox hBoxDownloads;
+
+    public EmailDetailsController(EmailMenager emailManager, ViewFactory viewFactory, String fxmlName) {
+        super(emailManager, viewFactory, fxmlName);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        EmailMessage emailMessage = emailMenager.getSelectedMessage();
+        subjectLabel.setText(emailMessage.getSubject());
+        senderLabel.setText(emailMessage.getSender());
+
+        MessageRendererService messageRendererService = new MessageRendererService(webView.getEngine());
+        messageRendererService.setEmailMessage(emailMessage);
+        messageRendererService.restart();
+
+    }
 }
